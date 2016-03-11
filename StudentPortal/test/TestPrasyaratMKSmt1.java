3d3099407d1611df3b4ca0d3ba7ea36942938be4
Michael William Kinsey <maiksem5@gmail.com>
2016-03-11 12:22:03 +0700

    perbaiki index out of bound

--- StudentPortal/test/TestPrasyaratMKSmt1.java
+++ StudentPortal/test/TestPrasyaratMKSmt1.java
@@ -59,10 +59,17 @@ public class TestPrasyaratMKSmt1 extends WithBrowser {
 			  browser.find(".form-control", withId("pw-input")).get(0).text(objFileConfReader.getPassSmt1());
 			  browser.find(".form-control", withName("submit")).get(0).click();
 			  browser.goTo("/prasyarat");
-			  FluentList<FluentWebElement> e1 = browser.find(".row").get(1).find("h5");
-			  if(e1.size()>0){
-				  assertEquals("PRASYARAT BELUM TERSEDIA", 
-						  browser.find(".row").get(1).find("h5").get(0).getText());
+			  FluentList<FluentWebElement> e1 = browser.find(".row");
+			
+			  if(e1.size()>1){
+				  FluentList<FluentWebElement> e2 = browser.find(".row").get(1).find("h5");
+				  if(e2.size()>0){
+					  assertEquals("PRASYARAT BELUM TERSEDIA", 
+							  browser.find(".row").get(1).find("h5").get(0).getText());
+				  }
+				  else{
+					  assertEquals("PRASYARAT BELUM TERSEDIA", "Test Gagal" );
+				  }
 			  }
 			  else{
 				  assertEquals("PRASYARAT BELUM TERSEDIA", "Test Gagal" );
